189aa11980feff25cf878698cff7d0d3b62d4be3
Burcu Dogan <burcud@google.com>
2013-10-31 17:41:13 +0100

    Fixing styling.

--- src/com/google/android/gms/drive/sample/querying/HomeActivity.java
+++ src/com/google/android/gms/drive/sample/querying/HomeActivity.java
@@ -47,198 +47,207 @@ import android.widget.Toast;
  *
  * @author jbd@google.com (Burcu Dogan)
  */
-public class HomeActivity extends BaseDriveActivity
-    implements OnItemClickListener, OnChildrenRetrievedCallback {
+public class HomeActivity extends BaseDriveActivity implements
+		OnItemClickListener, OnChildrenRetrievedCallback {
 
-  private static String TAG = "HomeActivity";
+	private static String TAG = "HomeActivity";
 
-  private static Query[] sQueries = new Query[] {
-    // files not shared with me
-    new Query.Builder().addFilters(Filters.not(
-        Filters.eq(SearchableField.mimeType(), "text/plain"))).build(),
+	private static Query[] sQueries = new Query[] {
+			// files not shared with me
+			new Query.Builder().addFilters(Filters.not(
+					Filters.eq(SearchableField.mimeType(), "text/plain")))
+					.build(),
 
-    // files shared with me
-    new Query.Builder().addFilters(Filters.sharedWithMe()).build(),
+			// files shared with me
+			new Query.Builder().addFilters(Filters.sharedWithMe()).build(),
 
-    // files with text/plain mimetype
-    new Query.Builder().addFilters(Filters.eq(SearchableField.mimeType(), "text/plain")).build(),
+			// files with text/plain mimetype
+			new Query.Builder().addFilters(
+					Filters.eq(SearchableField.mimeType(), "text/plain"))
+					.build(),
 
-    // files with a title containing 'a'
-    new Query.Builder().addFilters(Filters.contains(SearchableField.title(), "a")).build(),
+			// files with a title containing 'a'
+			new Query.Builder().addFilters(
+					Filters.contains(SearchableField.title(), "a")).build(),
 
-    // files starred and with text/plain mimetype
-    new Query.Builder().addFilters(Filters.and(
-        Filters.eq(SearchableField.mimeType(), "text/plain"),
-        Filters.eq(SearchableField.starred(), true))).build(),
+			// files starred and with text/plain mimetype
+			new Query.Builder().addFilters(Filters.and(
+					Filters.eq(SearchableField.mimeType(), "text/plain"),
+					Filters.eq(SearchableField.starred(), true))).build(),
 
-    // files with text/plain or text/html mimetype
-    new Query.Builder().addFilters(Filters.or(
-        Filters.eq(SearchableField.mimeType(), "text/html"),
-        Filters.eq(SearchableField.mimeType(), "text/plain"))).build()
-  };
+			// files with text/plain or text/html mimetype
+			new Query.Builder().addFilters(Filters.or(
+					Filters.eq(SearchableField.mimeType(), "text/html"),
+					Filters.eq(SearchableField.mimeType(), "text/plain")))
+					.build() };
 
-  /**
-   * User friendly titles for available queries.
-   */
-  private String[] mTitles;
+	/**
+	 * User friendly titles for available queries.
+	 */
+	private String[] mTitles;
 
-  /**
-   * Main drawer layout.
-   */
-  private DrawerLayout mMainDrawerLayout;
+	/**
+	 * Main drawer layout.
+	 */
+	private DrawerLayout mMainDrawerLayout;
 
-  /**
-   * List view that displays the available queries.
-   */
-  private ListView mListViewQueries;
+	/**
+	 * List view that displays the available queries.
+	 */
+	private ListView mListViewQueries;
 
-  /**
-   * List view that displays the query results.
-   */
-  private ListView mListViewFiles;
+	/**
+	 * List view that displays the query results.
+	 */
+	private ListView mListViewFiles;
 
-  /**
-   * Index of the selected query.
-   */
-  private int mSelectedIndex = 0;
+	/**
+	 * Index of the selected query.
+	 */
+	private int mSelectedIndex = 0;
 
-  /**
-   * Retrieved metadata results buffer or {@code null} if there are
-   * no results retrieved yet.
-   */
-  private MetadataBuffer mMetadataBuffer;
+	/**
+	 * Retrieved metadata results buffer or {@code null} if there are no results
+	 * retrieved yet.
+	 */
+	private MetadataBuffer mMetadataBuffer;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_home);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
 
-    mTitles = getResources().getStringArray(R.array.titles_array);
+		mTitles = getResources().getStringArray(R.array.titles_array);
 
-    mListViewQueries = (ListView) findViewById(R.id.listViewQueries);
-    mListViewQueries.setOnItemClickListener(this);
+		mListViewQueries = (ListView) findViewById(R.id.listViewQueries);
+		mListViewQueries.setOnItemClickListener(this);
 
-    mListViewFiles = (ListView) findViewById(R.id.listViewFiles);
-    mListViewFiles.setAdapter(new ResultsAdapter());
-    mListViewFiles.setEmptyView(findViewById(R.id.viewEmpty));
+		mListViewFiles = (ListView) findViewById(R.id.listViewFiles);
+		mListViewFiles.setAdapter(new ResultsAdapter());
+		mListViewFiles.setEmptyView(findViewById(R.id.viewEmpty));
 
-    // enable action bar for home button, so we can open
-    // the left list view for navigation.
-    getActionBar().setDisplayHomeAsUpEnabled(true);
-    getActionBar().setHomeButtonEnabled(true);
-    mMainDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayoutMain);
-  }
+		// enable action bar for home button, so we can open
+		// the left list view for navigation.
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		mMainDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayoutMain);
+	}
 
-  /**
-   * Called when {@code GoogleApiClient} is connected, no querying or client related
-   * actions other than disconnection should be invoked before.
-   */
-  @Override
-  public void onConnected(Bundle connectionHint) {
-    super.onConnected(connectionHint);
-    mListViewQueries.setAdapter(
-        new ArrayAdapter<String>(this, R.layout.row_query, mTitles));
-    refresh();
-  }
+	/**
+	 * Called when {@code GoogleApiClient} is connected, no querying or client
+	 * related actions other than disconnection should be invoked before.
+	 */
+	@Override
+	public void onConnected(Bundle connectionHint) {
+		super.onConnected(connectionHint);
+		mListViewQueries.setAdapter(
+				new ArrayAdapter<String>(this, R.layout.row_query, mTitles));
+		refresh();
+	}
 
-  /**
-   * Invokes calls to query user's Google Drive root folder's children
-   * with the currently selected query.
-   */
-  private void refresh() {
-    DriveFolder root = Drive.DriveApi.getRootFolder();
-    root.queryChildren(mGoogleApiClient, sQueries[mSelectedIndex]).addResultCallback(this);
-  }
+	/**
+	 * Invokes calls to query user's Google Drive root folder's children with
+	 * the currently selected query.
+	 */
+	private void refresh() {
+		DriveFolder root = Drive.DriveApi.getRootFolder();
+		root.queryChildren(mGoogleApiClient, sQueries[mSelectedIndex])
+				.addResultCallback(this);
+	}
 
-  /**
-   * Called when user interacts with the action bar. Handles home clicks
-   * to open the navigation drawer for the query selection list view.
-   */
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId() == android.R.id.home) {
-      mMainDrawerLayout.openDrawer(Gravity.LEFT);
-    }
-    return super.onOptionsItemSelected(item);
-  }
+	/**
+	 * Called when user interacts with the action bar. Handles home clicks to
+	 * open the navigation drawer for the query selection list view.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			mMainDrawerLayout.openDrawer(Gravity.LEFT);
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-  /**
-   * Called when user clicks on one of the items on the queries list.
-   * Keep the query's index and invoke to filter the user's Drive with
-   * the selected query.
-   */
-  @Override
-  public void onItemClick(AdapterView<?> arg0, View arg1, int i, long arg3) {
-    mMainDrawerLayout.closeDrawers();
-    mSelectedIndex = i;
-    refresh();
-  }
+	/**
+	 * Called when user clicks on one of the items on the queries list. Keep the
+	 * query's index and invoke to filter the user's Drive with the selected
+	 * query.
+	 */
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int i, long arg3) {
+		mMainDrawerLayout.closeDrawers();
+		mSelectedIndex = i;
+		refresh();
+	}
 
-  /**
-   * Called when query has executed and a result has been retrieved.
-   * Files list view should be re-rendered with the new results.
-   */
-  @Override
-  public void onChildrenRetrieved(MetadataBufferResult result) {
-    if (!result.getStatus().isSuccess()) {
-      Toast.makeText(this, R.string.msg_errorretrieval, Toast.LENGTH_SHORT).show();
-      return;
-    }
-    Log.d(TAG, "Retrieved file count: " + result.getMetadataBuffer().getCount());
-    mMetadataBuffer = result.getMetadataBuffer();
-    ((ResultsAdapter)mListViewFiles.getAdapter()).notifyDataSetChanged();
-  }
+	/**
+	 * Called when query has executed and a result has been retrieved. Files
+	 * list view should be re-rendered with the new results.
+	 */
+	@Override
+	public void onChildrenRetrieved(MetadataBufferResult result) {
+		if (!result.getStatus().isSuccess()) {
+			Toast.makeText(this, R.string.msg_errorretrieval,
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
+		Log.d(TAG, "Retrieved file count: "
+				+ result.getMetadataBuffer().getCount());
+		mMetadataBuffer = result.getMetadataBuffer();
+		((ResultsAdapter) mListViewFiles.getAdapter()).notifyDataSetChanged();
+	}
 
-  /**
-   * List adapter to provide data to the files list view. If there are
-   * no results yet retrieved, it shows no items.
-   */
-  private class ResultsAdapter extends BaseAdapter {
+	/**
+	 * List adapter to provide data to the files list view. If there are no
+	 * results yet retrieved, it shows no items.
+	 */
+	private class ResultsAdapter extends BaseAdapter {
+		/**
+		 * Returns the number of the items in {@code mMetaBuffer}, returns 0 if
+		 * it is {@code null}.
+		 */
+		@Override
+		public int getCount() {
+			if (mMetadataBuffer == null) {
+				return 0;
+			}
+			return mMetadataBuffer.getCount();
+		}
 
-    /**
-     * Returns the number of the items in {@code mMetaBuffer}, returns 0
-     * if it is {@code null}.
-     */
-    @Override
-    public int getCount() {
-      if (mMetadataBuffer == null) {
-        return 0;
-      }
-      return mMetadataBuffer.getCount();
-    }
+		/**
+		 * Returns the item at the ith position.
+		 */
+		@Override
+		public Object getItem(int i) {
+			return mMetadataBuffer.get(i);
+		}
 
-    /**
-     * Returns the item at the ith position.
-     */
-    @Override
-    public Object getItem(int i) {
-      return mMetadataBuffer.get(i);
-    }
+		/**
+		 * Returns the id of the item at the ith position.
+		 */
+		@Override
+		public long getItemId(int i) {
+			return i;
+		}
 
-    /**
-     * Returns the id of the item at the ith position.
-     */
-    @Override
-    public long getItemId(int i) {
-      return i;
-    }
+		/**
+		 * Inflates the row view for the item at the ith position, renders it
+		 * with the corresponding item.
+		 */
+		@Override
+		public View getView(int i, View convertView, ViewGroup arg2) {
+			if (convertView == null) {
+				convertView = View.inflate(getBaseContext(), R.layout.row_file,
+						null);
+			}
+			TextView titleTextView = (TextView) convertView.findViewById(
+					R.id.textViewTitle);
+			TextView descTextView = (TextView) convertView.findViewById(
+					R.id.textViewDescription);
+			Metadata metadata = mMetadataBuffer.get(i);
 
-    /**
-     * Inflates the row view for the item at the ith position, renders it
-     * with the corresponding item.
-     */
-    @Override
-    public View getView(int i, View convertView, ViewGroup arg2) {
-      if (convertView == null) {
-        convertView = View.inflate(getBaseContext(), R.layout.row_file, null);
-      }
-      TextView titleTextView = (TextView) convertView.findViewById(R.id.textViewTitle);
-      TextView descTextView = (TextView) convertView.findViewById(R.id.textViewDescription);
-      Metadata metadata = mMetadataBuffer.get(i);
-
-      titleTextView.setText(metadata.getTitle());
-      descTextView.setText(metadata.getModifiedDate().toString());
-      return convertView;
-    }
-  }
+			titleTextView.setText(metadata.getTitle());
+			descTextView.setText(metadata.getModifiedDate().toString());
+			return convertView;
+		}
+	}
 }
